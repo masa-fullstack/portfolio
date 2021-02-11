f43e791baad59414ca9f04969cdd1203655b7097
masa-fullstack <ishida.masahidee@gmail.com>
2021-02-11 21:17:45 +0900

    pagenation scroll into view

--- app/components/Pagenation.tsx
+++ app/components/Pagenation.tsx
@@ -1,6 +1,5 @@
 import { withStyles } from '@material-ui/core/styles'
 import MuiPagination from '@material-ui/lab/Pagination'
-import { useCallback, useEffect, useRef } from 'react'
 import { Action } from './Portfolios'
 
 type Props = {
@@ -22,35 +21,14 @@ const Pagination = withStyles({
 
 const Pagenation = ({ maxPage, page, dispatch }: Props) => {
   // console.log('Pagenation rendering')
-  const ref = useRef<HTMLDivElement>()
-  const scrollToBottomOfList = useCallback(() => {
-    ref.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'end',
-    })
-  }, [ref])
-
-  const isFirstRender = useRef(false)
-  useEffect(() => {
-    isFirstRender.current = true
-  }, [])
-  useEffect(() => {
-    if (isFirstRender) {
-      scrollToBottomOfList()
-    }
-  })
-
   return (
     <div style={{ textAlign: 'center' }}>
       <Pagination
         count={maxPage} //総ページ数
         color="primary" //ページネーションの色
-        onChange={(e, page) => {
-          dispatch({ type: 'SET_PAGE', value: page })
-        }} //変更されたときに走る関数。第2引数にページ番号が入る
+        onChange={(e, page) => dispatch({ type: 'SET_PAGE', value: page })} //変更されたときに走る関数。第2引数にページ番号が入る
         page={page} //現在のページ番号
       />
-      <div ref={ref} />
     </div>
   )
 }
