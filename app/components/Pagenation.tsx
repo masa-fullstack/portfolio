417f280162464439f080385ac1c8e7bef19ffcc1
masa-fullstack <ishida.masahidee@gmail.com>
2021-02-11 21:13:17 +0900

    pagenation scroll into view

--- app/components/Pagenation.tsx
+++ app/components/Pagenation.tsx
@@ -1,6 +1,6 @@
 import { withStyles } from '@material-ui/core/styles'
 import MuiPagination from '@material-ui/lab/Pagination'
-import { createRef, useCallback } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { Action } from './Portfolios'
 
 type Props = {
@@ -22,13 +22,23 @@ const Pagination = withStyles({
 
 const Pagenation = ({ maxPage, page, dispatch }: Props) => {
   // console.log('Pagenation rendering')
-  const ref = createRef<HTMLDivElement>()
+  const ref = useRef<HTMLDivElement>()
   const scrollToBottomOfList = useCallback(() => {
     ref.current.scrollIntoView({
       behavior: 'smooth',
-      block: 'nearest',
+      block: 'end',
     })
   }, [ref])
+
+  const isFirstRender = useRef(false)
+  useEffect(() => {
+    isFirstRender.current = true
+  }, [])
+  useEffect(() => {
+    if (isFirstRender) {
+      scrollToBottomOfList()
+    }
+  })
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -37,7 +47,6 @@ const Pagenation = ({ maxPage, page, dispatch }: Props) => {
         color="primary" //ページネーションの色
         onChange={(e, page) => {
           dispatch({ type: 'SET_PAGE', value: page })
-          scrollToBottomOfList()
         }} //変更されたときに走る関数。第2引数にページ番号が入る
         page={page} //現在のページ番号
       />
